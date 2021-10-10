407faf769a91a91eeba3819d885a0e5726698301
Evgeniy <60233967+mifeex@users.noreply.github.com>
2021-10-10 14:45:24 +0300

    Add files via upload

---
+++ precache-manifest.a1a63cfbd8f11a2b7f78ae51beabec2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec15e20b34a74d1b9fe9e92872a55ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d1da424ba4db4bc6a4",
+    "url": "/static/css/2.774b0c34.chunk.css"
+  },
+  {
+    "revision": "4456e8af9e88d53eec50",
+    "url": "/static/css/main.5687c01c.chunk.css"
+  },
+  {
+    "revision": "f4d1da424ba4db4bc6a4",
+    "url": "/static/js/2.94947055.chunk.js"
+  },
+  {
+    "revision": "4456e8af9e88d53eec50",
+    "url": "/static/js/main.2ce949d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
